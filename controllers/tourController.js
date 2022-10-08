bc5003fb5e8cd3a903838bbecb18e8bb6f85ada6
PrincyChauhan <chauhanprincee7@gmail.com>
2022-10-08 14:48:10 +0530

    Apply Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,6 +33,20 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4.Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = Tour.countDocuments();
+      if (skip > numTours) {
+        throw new Error('Page Not Exist');
+      }
+    }
+
     // Execute the query
     const tours = await query;
 
