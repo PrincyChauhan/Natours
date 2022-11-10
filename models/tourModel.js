058d3324c1c6958bc6a272b9a1b3deed6c6a899a
PrincyChauhan <chauhanprincee7@gmail.com>
2022-11-10 15:34:31 +0530

    bulit-in-validator

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,7 +7,9 @@ const tourSchema = mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
-      trim: true
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 charaters'],
+      minlength: [10, 'A tour name must have less or equal then 10 charaters']
     },
     slug: String,
     duration: {
@@ -20,11 +22,16 @@ const tourSchema = mongoose.Schema(
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
-    },
+      required: [true, 'A tour must have a difficulty'],
+      enum:{
+        values:['easy','medium','difficult'],
+        message:'Difficulty is either easy,medium or difficult'
+    }},
     reatingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0 '],
+      max: [5, 'Rating must be below 5.0 ']
     },
     reatingsQuantity: {
       type: Number,
