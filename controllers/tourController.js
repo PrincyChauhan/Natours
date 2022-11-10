db1caec2e1b1f469d97f46f1e954e3f24d0c3dfd
PrincyChauhan <chauhanprincee7@gmail.com>
2022-11-10 11:38:10 +0530

    min,max,avg,sum

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -109,28 +109,19 @@ exports.getTourStas = async (req, res) => {
   try {
     const stats = await Tour.aggregate([
       {
-        $match: { ratingsAverage: { $gte: 4.5 } }
+        $match: { reatingsAverage: { $gte: 4.5 } }
       },
       {
         $group: {
-          // _id: '$ratingsAverage',
           _id: { $toUpper: '$difficulty' },
           numTours: { $sum: 1 },
           numRatings: { $sum: '$ratingsQuantity' },
-          avgRating: { $avg: '$ratingsAverage' },
+          avgRating: { $avg: '$reatingsAverage' },
           avgPrice: { $avg: '$price' },
-          minprice: { $min: '$price' },
-          maxprice: { $min: '$price' }
+          maxPrice: { $max: '$price' },
+          minPrice: { $min: '$price' }
         }
-      },
-      // {
-      //   $sort: { avgPrice: 1 }
-      // },
-      // {
-      //   $match: {
-      //     _id: { $ne: 'EASY' }
-      //   }
-      // }
+      }
     ]);
     res.status(200).json({
       status: 'success',
